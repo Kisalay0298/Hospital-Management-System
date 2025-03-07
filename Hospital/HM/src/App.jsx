3681a105ff15889e8afd918d7d88a5600c858221
Kisalay Komal <100374713+Kisalay0298@users.noreply.github.com>
2025-03-07 12:43:26 +0530

    Update App.jsx

--- Hospital/HM/src/App.jsx
+++ Hospital/HM/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
