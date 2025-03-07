17d8c2601fda32e251778c9930812a5b653f2255
Kisalay Komal <100374713+Kisalay0298@users.noreply.github.com>
2025-03-07 13:57:19 +0530

    Update Login.jsx

--- Hospital/HM/src/pages/Login.jsx
+++ Hospital/HM/src/pages/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
           <div>Password</div>
           <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
         </div>
-        <button type="submit" className="bg-primary text-white w-full py-2 rounded-md text-base">{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        <button type="submit" className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer">{state === 'Sign Up' ? "Create Account" : "Login"}</button>
         {
           state === 'Sign Up'
           ? <p>Already have an account? <span onClick={() => setState('Login')} className="text-primary underline cursor-pointer">Login here</span></p>
